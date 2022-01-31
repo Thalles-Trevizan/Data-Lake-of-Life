345d5f3d55a54c1e21cc40704be5468e41f7aa8e
Thalles Trevizan <thalles_trevizan@hotmail.com>
2022-01-30 23:20:23 -0300

    Add Backend in Porject

---
+++ Backend/src/main/java/com/example/dloflife/DataLakeOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dloflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLakeOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLakeOfLifeApplication.class, args);
+	}
+
+}
